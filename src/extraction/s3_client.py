00001a13cac693cd13ad4fb49a3bb0e1c50113bf
jackmolomo <jackmolomo@gmail.com>
2026-08-22 19:44:22 +0200

    added the config to reference aws config

--- src/extraction/s3_client.py
+++ src/extraction/s3_client.py
@@ -1,39 +1,30 @@
 import io
-
 import boto3
 import pandas as pd
 from botocore import UNSIGNED
 from botocore.config import Config
-import yaml
+from src.config import config
 
 
 class S3Client:
-    """Client for reading public City of Cape Town S3 objects."""
 
     def __init__(self):
-        configpath = "config/aws.yaml"
-        with open(configpath, "r") as file:
-            aws_config = yaml.safe_load(file)
+        self.bucket = config["s3"]["bucket"]
 
-            self.bucket = aws_config["bucket"]
-            self.region = aws_config["region"] 
-            self.client = boto3.client("s3",region_name=self.region, config=Config(signature_version=UNSIGNED))
-        
-
-  
-
-    def read_csv(self, key: str, **kwargs) -> pd.DataFrame:
-        """Read a CSV or compressed CSV from the public S3 bucket."""
-
-        response = self.client.get_object(
-            Bucket=self.bucket,
-            Key=key,
+        self.client = boto3.client(
+            "s3",
+            region_name=config["aws"]["region"],
+            config=Config(signature_version=UNSIGNED),
         )
 
-        file_stream = response["Body"].read()
+    def read_csv(self, key, **kwargs):
+        response = self.client.get_object(
+            Bucket=self.bucket,
+            Key=key
+        )
 
         return pd.read_csv(
-            io.BytesIO(file_stream),
+            io.BytesIO(response["Body"].read()),
             compression="gzip",
-            **kwargs,
+            **kwargs
         )
