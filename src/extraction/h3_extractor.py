c1899711490a1204dff83fc03595f570d10629db
jackmolomo <jackmolomo@gmail.com>
2026-08-22 21:39:34 +0200

    adding the h3 py fileand notebook

---
+++ src/extraction/h3_extractor.py
@@ -0,0 +1,20 @@
+import json
+from src.extraction.s3_client import S3Client
+
+
+class H3Extractor:
+
+    def __init__(self):
+        self.s3 = S3Client()
+
+    def extract_resolution_8(self, key):
+
+        data = json.loads(
+            self.s3.read_geojson(key)
+        )
+
+        return [
+            feature
+            for feature in data["features"]
+            if feature["properties"]["resolution"] == 8
+        ]
